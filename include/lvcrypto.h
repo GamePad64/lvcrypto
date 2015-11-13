3f2f08eb153e6dec4d60e0296db3544b568ac5e0
Alexander Shishenko <alex@shishenko.com>
2015-11-13 07:11:59 +0300

    Added lvcrypto as proper CMake-based library

---
+++ include/lvcrypto.h
@@ -0,0 +1,23 @@
+/* Written in 2015 by Alexander Shishenko <alex@shishenko.com>
+ *
+ * LVCrypto - Cryptographic wrapper, used in Librevault.
+ * To the extent possible under law, the author(s) have dedicated all copyright
+ * and related and neighboring rights to this software to the public domain
+ * worldwide. This software is distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+#pragma once
+#include <lvcrypto/AES_CBC.h>
+#include <lvcrypto/Base32.h>
+#include <lvcrypto/Base58.h>
+#include <lvcrypto/Base64.h>
+#include <lvcrypto/CRC32.h>
+#include <lvcrypto/Hex.h>
+#include <lvcrypto/HMAC-SHA3.h>
+#include <lvcrypto/LuhnModN.h>
+#include <lvcrypto/SHA2.h>
+#include <lvcrypto/SHA3.h>
+
+#include <lvcrypto/Transformer.h>
